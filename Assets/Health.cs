ad4e1574959883312fc58c0393bf3f771850871c
Joe Rutkin <rutkinj@gmail.com>
2023-05-20 15:51:07 -0700

    basic hp and death

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] int hitpoints = 1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] Animator anim;
+
+    public void TakeDamage(int damage){
+        hitpoints -= damage;
+
+        if (hitpoints <= 0){
+            Die();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Die(){
+        anim.SetTrigger("die");
+        Destroy(gameObject);
     }
+
 }
