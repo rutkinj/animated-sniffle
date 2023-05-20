2a4cb24ee9d4d70ca9d8ea5f9dad69d8c81d2680
Joe Rutkin <rutkinj@gmail.com>
2023-05-20 09:50:17 -0700

    weapon script; broadcasts to input manager

---
+++ Assets/Blammer.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Blammer : MonoBehaviour
+{
+    void OnFire(InputValue value){
+        print("bingbong");
+    }
+}
