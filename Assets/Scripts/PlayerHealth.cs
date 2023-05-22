f7ec8022090267d9452db8560f011d72521813ab
Joe Rutkin <rutkinj@gmail.com>
2023-05-22 15:42:28 -0700

    connect player health to simple hud

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerHealth : MonoBehaviour
 {
-  [SerializeField] int hitpoints = 5;
-//   [SerializeField] Animator anim;
-    bool isDead = false;
+  [SerializeField] int maxHitPoints = 5;
+  [SerializeField] TextMeshProUGUI healthDisplay;
+  //   [SerializeField] Animator anim;
+  int hitpoints = 0;
+  bool isDead = false;
+
+  private void Awake()
+  {
+    hitpoints = maxHitPoints;
+    UpdateDisplay();
+  }
 
   public void TakeDamage(int damage)
   {
     hitpoints -= damage;
+    UpdateDisplay();
 
     if (hitpoints <= 0)
     {
@@ -24,7 +34,13 @@ public class PlayerHealth : MonoBehaviour
     isDead = true;
   }
 
-  public bool IsDead(){
+  public bool IsDead()
+  {
     return isDead;
+  }
+
+  private void UpdateDisplay()
+  {
+    healthDisplay.text = hitpoints.ToString();
   }
 }
