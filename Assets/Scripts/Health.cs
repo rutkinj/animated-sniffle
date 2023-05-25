9763a6d72f2ba974a663db642cb40d39bb0b8abc
Joe Rutkin <rutkinj@gmail.com>
2023-05-25 11:58:14 -0700

    add sfx to enemies

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -6,10 +6,15 @@ public class Health : MonoBehaviour
 {
     [SerializeField] int hitpoints = 1;
     [SerializeField] Animator anim;
+    [SerializeField] AudioSource sfx;
+    [SerializeField] AudioClip deathSFX;
+    [SerializeField] AudioClip hitSFX;
+    [SerializeField] float minPitch = -1;
+    [SerializeField] float maxPitch = 2;
 
     public void TakeDamage(int damage){
         hitpoints -= damage;
-
+        AudioRandomPitch(hitSFX);
         if (hitpoints <= 0){
             Die();
         }
@@ -17,8 +22,14 @@ public class Health : MonoBehaviour
 
     private void Die(){
         anim.SetTrigger("die");
+        AudioRandomPitch(deathSFX);
         GetComponent<BoxCollider>().enabled = false;
         Destroy(gameObject, 1f);
+    }
+
+    private void AudioRandomPitch(AudioClip clip){
+        sfx.pitch = Random.Range(minPitch, maxPitch);
+        sfx.PlayOneShot(clip);
     }
 
 }
