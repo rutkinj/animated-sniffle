4e9e4c9a5ce8db7c5cbfc6d95f7576c886218fca
Joe Rutkin <rutkinj@gmail.com>
2023-05-26 16:23:08 -0700

    attach key inv to hud with ducktape

--- Assets/Scripts/Lock & Key/Inventory.cs
+++ Assets/Scripts/Lock & Key/Inventory.cs
@@ -1,14 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
+    [SerializeField] Image red;
+    [SerializeField] Image blue;
+    [SerializeField] Image green;
     List<KeyType> keys = new List<KeyType>();
 
     public void AddKey(KeyType keyType)
     {
         keys.Add(keyType);
+        //truly shameful but I'm running out of time lol
+        if(keyType == KeyType.Red){
+            red.enabled = true;
+        }
+        else if(keyType == KeyType.Green){
+            green.enabled = true;
+        }
+        else if (keyType == KeyType.Blue){
+            blue.enabled = true;
+        }
     }
 
     public void RemoveKey(KeyType keyType)
