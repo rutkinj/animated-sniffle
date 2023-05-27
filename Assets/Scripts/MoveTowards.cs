1e59f6f1fa59fdd8b3778525d5c5c89502447897
Joe Rutkin <rutkinj@gmail.com>
2023-05-26 18:20:56 -0700

    disable unused code

--- Assets/Scripts/MoveTowards.cs
+++ Assets/Scripts/MoveTowards.cs
@@ -5,8 +5,8 @@ using UnityEngine.AI;
 
 public class MoveTowards : MonoBehaviour
 {
-  [SerializeField] float speed = 3f;
-  [SerializeField] float moveDelay = 1f;
+  // [SerializeField] float speed = 3f;
+  // [SerializeField] float moveDelay = 1f;
   public bool canMove = true;
   float step;
   Transform player = null;
@@ -32,52 +32,52 @@ public class MoveTowards : MonoBehaviour
     agent.SetDestination(player.position);
   }
 
-  void Move2d()
-  {
-    print("started move 2d");
-    // while (!Mathf.Approximately(player.position.x, transform.position.x) &&
-    // !Mathf.Approximately(player.position.z, transform.position.z))
-    // {
-    step = speed * Time.deltaTime;
-    float xDist = player.position.x - transform.position.x;
-    float zDist = player.position.z - transform.position.z;
+  // void Move2d()
+  // {
+  //   print("started move 2d");
+  //   // while (!Mathf.Approximately(player.position.x, transform.position.x) &&
+  //   // !Mathf.Approximately(player.position.z, transform.position.z))
+  //   // {
+  //   step = speed * Time.deltaTime;
+  //   float xDist = player.position.x - transform.position.x;
+  //   float zDist = player.position.z - transform.position.z;
 
-    bool moveX = Mathf.Abs(xDist) > Mathf.Abs(zDist);
+  //   bool moveX = Mathf.Abs(xDist) > Mathf.Abs(zDist);
 
-    if (moveX)
-    {
-      print("moving x");
-      //move x
-      if (xDist > 0)
-      {
-        print("moving right");
-        transform.Translate(Vector3.right * step, Space.World);
-      }
-      else
-      {
-        print("moving left");
+  //   if (moveX)
+  //   {
+  //     print("moving x");
+  //     //move x
+  //     if (xDist > 0)
+  //     {
+  //       print("moving right");
+  //       transform.Translate(Vector3.right * step, Space.World);
+  //     }
+  //     else
+  //     {
+  //       print("moving left");
 
-        transform.Translate(Vector3.left * step, Space.World);
-      }
-    }
-    else
-    {
-      //move z
-      print("moving z");
-      if (zDist < 0)
-      {
-        print("moving back");
+  //       transform.Translate(Vector3.left * step, Space.World);
+  //     }
+  //   }
+  //   else
+  //   {
+  //     //move z
+  //     print("moving z");
+  //     if (zDist < 0)
+  //     {
+  //       print("moving back");
 
-        transform.Translate(Vector3.back * step, Space.World);
-      }
-      else
-      {
-        print("moving fwd");
+  //       transform.Translate(Vector3.back * step, Space.World);
+  //     }
+  //     else
+  //     {
+  //       print("moving fwd");
 
-        transform.Translate(Vector3.forward * step, Space.World);
-      }
-    }
-  }
+  //       transform.Translate(Vector3.forward * step, Space.World);
+  //     }
+  //   }
+  // }
 
 
 }
