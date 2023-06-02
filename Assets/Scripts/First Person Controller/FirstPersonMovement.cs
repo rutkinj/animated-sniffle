ee5552f9d76136d12e421a7326a780aa591cba26
Joe Rutkin <rutkinj@gmail.com>
2023-06-02 15:12:25 -0700

    fix infinite speed sprinting

--- Assets/Scripts/First Person Controller/FirstPersonMovement.cs
+++ Assets/Scripts/First Person Controller/FirstPersonMovement.cs
@@ -3,49 +3,60 @@ using UnityEngine.InputSystem;
 
 public class FirstPersonMovement : MonoBehaviour
 {
-    [SerializeField] float speed = 5;
-    float currentSpeed;
-    Vector2 velocity;
-    Vector2 rawInput;
-    bool frozen;
-    PlayerHealth health;
+  [SerializeField] float speed = 5;
+  float currentSpeed;
+  Vector2 velocity;
+  Vector2 rawInput;
+  bool frozen;
+  PlayerHealth health;
 
-    void Awake(){
+  void Awake()
+  {
+    currentSpeed = speed;
+    health = GetComponent<PlayerHealth>();
+  }
+
+  void FixedUpdate()
+  {
+    if (frozen || health.IsDead()) return;
+    MoveCharacter();
+  }
+
+  void MoveCharacter()
+  {
+    velocity.x = rawInput.x * currentSpeed * Time.deltaTime;
+    velocity.y = rawInput.y * currentSpeed * Time.deltaTime;
+    transform.Translate(velocity.x, 0, velocity.y);
+  }
+
+  void OnMove(InputValue value)
+  {
+    rawInput = value.Get<Vector2>();
+  }
+
+  void OnRun(InputValue value)
+  {
+    if(value.isPressed){
+        currentSpeed = speed * 2;
+    }
+    if(!value.isPressed){
         currentSpeed = speed;
-        health = GetComponent<PlayerHealth>();
     }
 
-    void FixedUpdate()
-    {
-        if(frozen || health.IsDead())return;
-        MoveCharacter();
-    }
+  }
 
-    void MoveCharacter()
-    {
-        velocity.x = rawInput.x * currentSpeed * Time.deltaTime;
-        velocity.y = rawInput.y * currentSpeed * Time.deltaTime;
-        transform.Translate(velocity.x, 0, velocity.y);
-    }
+  public void alterSpeed(float multiplier = 1)
+  {
+    currentSpeed = speed * multiplier;
+  }
 
-    void OnMove(InputValue value)
-    {
-        rawInput = value.Get<Vector2>();
-    }
+  public void Freeze()
+  {
+    frozen = true;
+  }
 
-    void OnRun(){
-        rawInput *= new Vector2(2,2);
-    }
-
-    public void alterSpeed(float multiplier = 1){
-        currentSpeed = speed * multiplier;
-    }
-
-    public void Freeze(){
-        frozen = true;
-    }
-
-    public bool isFrozen(){
-        return frozen;
-    }
+  public bool isFrozen()
+  {
+    return frozen;
+  }
 }
